6a902ba004645362e6f9845c36037c6f4b2d7142
Виктор Кочнев <victor_k02@mail.ru>
2023-11-16 20:30:49 +0300

    release/1.8.0

--- DAL/OrderC.cs
+++ DAL/OrderC.cs
@@ -11,7 +11,9 @@ namespace DAL
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Order")]
     public partial class OrderC
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
